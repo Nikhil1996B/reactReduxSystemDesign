89ba50e6e6bdaa5cd59be7ed28f019a9644ce883
Nikhil Bhaskar <nikhil.b@sonata-software.com>
2020-10-30 12:45:59 +0530

    Parsing the http requests

--- src/reducers/Actionpaylaod.js
+++ src/reducers/Actionpaylaod.js
@@ -1,6 +1,22 @@
+import { DATA_LOADED, DATA_LOADING } from "../actions/Actions"
+
 // fetch the users from API
+const __checkStatus = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    let error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+};
+const __parseJSON = response => {
+  return response.json();
+};
+
+
 export const fetchData = () =>
   fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((posts) => posts)
+    .then(__checkStatus)
+    .then(__parseJSON)
     .catch((err) => console.log("Somethin went wrong!", err));
